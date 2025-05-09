9ec9b6b4033385924c83c9bf1301ca031ffc75a9
svk-123 <vinoku89@gmail.com>
2025-05-10 00:20:16 +0800

    try border for higher score

--- gemini_20_async_api_call.py
+++ gemini_20_async_api_call.py
@@ -96,8 +96,8 @@ async def main_batch_processor():
     Main async function to load data, iterate through batches,
     process each batch asynchronously, and save results.
     """
-    df = pd.read_csv('./drawing-with-llms/description_master_gemini_20_48k.csv')
-    df = df.iloc[20000:] # Use a smaller sample for testing
+    df = pd.read_csv('./drawing-with-llms/description_master_gpt_deepseek_10k_v1.csv')
+    #df = df.iloc[20000:] # Use a smaller sample for testing
 
     batch_size = 100
     total_rows = len(df)
